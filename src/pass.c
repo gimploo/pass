3558c8108c976d27875c191311a1340fa83e2d72
gimploo <gokulkrishnan949@gmail.com>
2021-08-21 11:49:02 +0530

    Implemented a new function that outputs all occurance instead of just the first

--- src/pass.c
+++ src/pass.c
@@ -18,7 +18,7 @@ const char* usage =
 
 
 
-void parse_csv_file(CSV *csv, bool flag)
+void parse_csv_get_first_occurance(CSV *csv, bool flag)
 {
     if (csv == NULL) return ;
 
@@ -50,6 +50,54 @@ void parse_csv_file(CSV *csv, bool flag)
     if (flag == true) {
         str_print(row->buffer[header->header_count - 1]);
         printf("\n");
+    }
+
+    csv_row_destroy(row);
+}
+
+void parse_csv_get_all_occurance(CSV *csv, bool flag)
+{
+    if (csv == NULL) return ;
+
+    char user_input[KB];
+    memset(user_input, 0, KB);
+    printf("[?] Enter website name: ");
+    scanf("%s", user_input);
+
+    if (strcmp(user_input, "/quit") == 0) return ;
+
+    str_t find = new_str(user_input);
+
+    size_t line_num_array[csv->entries.entry_count];
+    stack_t stack = stack_init((void **)line_num_array, csv->entries.entry_count);
+    csv_get_all_line_nums_of_string_restricted_to_a_header_field(csv, &stack, 1, &find);
+
+
+    size_t line_num = 0;
+    for_i_in_stack(&stack)
+    {
+        line_num = line_num_array[i];
+
+        Row *row = csv_get_row_from_line_number(csv, line_num);
+        assert(row);
+
+        Header *header = &csv->header;
+        assert(header);
+            
+        printf("\n");
+        for (size_t i = 0; i < csv->header.header_count && flag == false; i++)
+        {
+            printf("[!] "STR_FMT": \t"STR_FMT"\n", 
+                    STR_ARG(header->header[i]), 
+                    STR_ARG(row->buffer[i]));
+        }
+
+        if (flag == true) {
+            str_print(row->buffer[header->header_count - 1]);
+            printf("\n");
+        }
+
+        csv_row_destroy(row);
     }
 }
 
@@ -86,13 +134,10 @@ int main(int argc, char *argv[])
 
     CSV csv = csv_init(csv_file);
 
-    parse_csv_file(&csv, flag);
+    parse_csv_get_all_occurance(&csv, flag);
     /*csv_print_all_entries(&csv);*/
 
 
-
-
-cleanup:
 
     csv_destroy(&csv);
 
